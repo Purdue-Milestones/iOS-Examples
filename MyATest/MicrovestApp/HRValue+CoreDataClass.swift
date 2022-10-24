a850e4f3167761832091cd0824a3c640e1df756c
Carolina Solís <carolinasolis@Carolinas-MacBook-Air.local>
2022-10-24 14:46:16 -0400

    Added CoreData

---
+++ MyATest/MicrovestApp/HRValue+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  HRValue+CoreDataClass.swift
+//  
+//
+//  Created by Carolina Solís on 10/24/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(HRValue)
+public class HRValue: NSManagedObject {
+
+}
