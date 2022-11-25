0a243515b61c2cf39479b3f609d648a2ababfc0a
Carolina Solís <carosol01@gmail.com>
2022-11-25 17:15:06 -0500

    Added changes

--- MyA copy 2.52.04 PM/MicrovestApp/VideoViewController.swift
+++ MyA copy 2.52.04 PM/MicrovestApp/VideoViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import AVKit
+import MobileCoreServices
 
-class VideoViewController: UIViewController {
+class VideoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //side menu button
     @IBOutlet weak var sideMenuBtn: UIBarButtonItem!
-    
+    //video switch (on or off)
     @IBOutlet weak var videoButton: UISwitch!
     @IBAction func videoSwitch(_ sender: UISwitch) {
         if sender.isOn{
@@ -23,25 +26,76 @@ class VideoViewController: UIViewController {
         }
     }
     
+    //Record and play videos
+    var videoAndImageReview = UIImagePickerController()
+    var videoURL : URL?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //show side menu
         sideMenuBtn.image = UIImage(systemName: "text.justify")
         sideMenuBtn.target = revealViewController()
         sideMenuBtn.action = #selector(revealViewController()?.revealSideMenu)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // recording Video
+    @IBAction func recordAct(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            print("Camera Available")
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = [UTType.movie.identifier]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("Camera Unavailable")
+        }
     }
-    */
-
+    
+       
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        guard let mediaType = info[UIImagePickerController.InfoKey.mediaURL.rawValue] as? String,
+                    mediaType == (UTType.movie.identifier),
+              let url = info[UIImagePickerController.InfoKey.mediaURL.rawValue] as? URL,
+                    UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+                    else {
+                        return
+                }
+                // Handle a movie capture
+                UISaveVideoAtPathToSavedPhotosAlbum(
+                    url.path,
+                    self,
+                    #selector(video(_:didFinishSavingWithError:contextInfo:)),
+                    nil)
+       }
+       
+       @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+           let title = (error == nil) ? "Success" : "Error"
+           let message = (error == nil) ? "Video was saved" : "Video failed to save"
+           
+           let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+           present(alert, animated: true, completion: nil)
+       }
+       
+       // playing Video
+    
+       
+    @IBAction func playAct(_ sender: UIButton) {
+        videoAndImageReview.sourceType = .savedPhotosAlbum
+        videoAndImageReview.delegate = self
+        videoAndImageReview.mediaTypes = ["public.movie"]
+        present(videoAndImageReview, animated: true, completion: nil)
+    }
+       
+       func videoAndImageReview(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+           videoURL = info[UIImagePickerController.InfoKey.mediaURL.rawValue] as? URL
+           print("\(String(describing: videoURL))")
+           self.dismiss(animated: true, completion: nil)
+       }
+    
 }
